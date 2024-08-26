efa0530d617b18d66169411c7d01059742181c34
HaoMAFRLu <hao.ma@tuebingen.mpg.de>
2024-08-26 13:29:11 +0200

    final test

--- test/generate_params.py
+++ test/generate_params.py
@@ -8,9 +8,10 @@ import utils as fcs
 root = fcs.get_parent_path(lvl=1)
 file = os.path.join(root, 'params.txt')
 value_w = ['0']
-value_y = [f"{10**-i:.0e}" for i in range(3, 9)]
-value_d = [f"{10**-i:.0e}" for i in range(3, 9)]
-value_ini = [f"{10**-i:.0e}" for i in range(3, 9)]
+# value_y = [f"{10**-i:.0e}" for i in range(3, 9)]
+value_y = ['1e-03']
+value_d = [f"{10**-i:.0e}" for i in range(3, 10)]
+value_ini = [f"{10**-i:.0e}" for i in range(3, 10)]
 
 combinations = itertools.product(value_w, value_y, value_d, value_ini)
 
